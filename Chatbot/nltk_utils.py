41aec3f265793bef4127485921f985e1d748bc9f
mariosconsta <mariosconsta@gmail.com>
2021-10-27 18:58:03 +0300

    Created nltk utility python file that contains our tokenize, stemming and bag of words functions for our data pre-processing.

---
+++ Chatbot/nltk_utils.py
@@ -0,0 +1,17 @@
+import nltk
+nltk.download('punkt')
+
+from nltk.stem.porter import PorterStemmer
+stemmer = PorterStemmer()
+
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+def stem(word):
+    return stemmer.stem(word.lower())
+
+def bow(tokenized_sentence, all_words):
+    pass
+
+
+
