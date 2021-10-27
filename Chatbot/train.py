162d785d93b28103119cf0d443d827632892e564
mariosconsta <mariosconsta@gmail.com>
2021-10-27 21:36:39 +0300

    Imported pytorch libraries

--- Chatbot/train.py
+++ Chatbot/train.py
@@ -2,9 +2,9 @@ import json
 from nltk_utils import tokenize, stem, bow
 import numpy as np
 
-# import torch
-# import torch.nn as nn
-# from torch.utils.data improt Dataset, DataLoader
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
 
 with open('intents.json', 'r') as f:
     intents = json.load(f)
