555888b11604fd9b0f75ffdea934c2945ab267b3
mariosconsta <mariosconsta@gmail.com>
2021-11-16 23:16:58 +0200

    Fixed variable name from intents to intent and also edited the json file for context application with set and filter functions.

--- Chatbot/chat.py
+++ Chatbot/chat.py
@@ -42,12 +42,11 @@ def response(msg, userID='123'):
     if probs.item() > 0.75:
         for intent in intents["intents"]:
             if tag == intent["tag"]:
-                if 'context_set' in intents:
-                    context[userID] = i['context_set']             
-                #return random.choice(intent['responses'])
+                if 'context_set' in intent:
+                    context[userID] = intent['context_set']
                 
-                if not 'context_filter' in intents or \
-                 (userID in context and 'context_filter' in i and i['context_filter'] == context[userID]):
+                if not 'context_filter' in intent or \
+                 (userID in context and 'context_filter' in intent and intent['context_filter'] == context[userID]):
                      return random.choice(intent['responses'])
                      
     return "I do not understand..."
